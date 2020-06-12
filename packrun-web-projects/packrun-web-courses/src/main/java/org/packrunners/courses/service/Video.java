171988b815caa9c38f8e73316c5ecd0588b1e3db
matthewru <matthewru2005@gmail.com>
2020-06-12 18:17:15 -0500

    Video Content done

--- packrun-web-projects/packrun-web-courses/src/main/java/org/packrunners/courses/service/Video.java
+++ packrun-web-projects/packrun-web-courses/src/main/java/org/packrunners/courses/service/Video.java
@@ -11,94 +11,85 @@ import java.util.List;
  */
 public class Video {
 
-    public static final String PROPERTY_NAME_DISPLAY_NAME = "name";
-    public static final String PROPERTY_NAME_AUTHOR = "author";
-    public static final String PROPERTY_NAME_COURSE = "course";
-    public static final String PROPERTY_NAME_LAST_MODIFIED_DATE = "date";
-    public static final String PROPERTY_NAME_ATTACHMENTS = "video";
-    public static final String PROPERTY_NAME_COURSE_TYPE = "courseType";
-    public static final String PROPERTY_NAME_SCHOOL = "school";
+  public static final String PROPERTY_NAME_DISPLAY_NAME = "name";
+  public static final String PROPERTY_NAME_AUTHOR = "author";
+  public static final String PROPERTY_NAME_COURSE = "course";
+  public static final String PROPERTY_NAME_LAST_MODIFIED_DATE = "date";
+  public static final String PROPERTY_NAME_ATTACHMENTS = "video";
+  public static final String PROPERTY_NAME_COURSE_TYPE = "courseType";
+  public static final String PROPERTY_NAME_SCHOOL = "school";
+  private String name;
+  private String author;
+  private List<Category> courses;
+  private List<Category> schools;
+  private Date lastModifiedDate;
+  private Asset video;
+  private String link;
+  private String identifier;
 
-    private String name;
-    private String author;
-    private String course;
-    private Date lastModifiedDate;
-    private Asset video;
-    private String link;
+  public String getLink() {
 
-    public String getLink() {
-        return link;
-    }
+    return link;
+  }
 
-    public void setLink(String link) {
-        this.link = link;
-    }
+  public void setLink(String link) {
+    this.link = link;
+  }
 
-    public String getIdentifier() {
-        return identifier;
-    }
+  public String getIdentifier() {
+    return identifier;
+  }
 
-    public void setIdentifier(String identifier) {
-        this.identifier = identifier;
-    }
+  public void setIdentifier(String identifier) {
+    this.identifier = identifier;
+  }
 
-    private String identifier;
-    private List<Category> schools;
-    private List<Category> courseTypes;
+  public String getName() {
+    return name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getAuthor() {
+    return author;
+  }
 
-    public String getAuthor() {
-        return author;
-    }
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+  public List<Category> getCourses() {
+    return courses;
+  }
 
-    public String getCourse() {
-        return course;
-    }
+  public void setCourses(List<Category> courses) {
+    this.courses = courses;
+  }
 
-    public void setCourse(String course) {
-        this.course = course;
-    }
+  public Date getLastModifiedDate() {
+    return lastModifiedDate;
+  }
 
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
+  public void setLastModifiedDate(Date lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
 
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
+  public Asset getVideo() {
+    return video;
+  }
 
-    public Asset getVideo() {
-        return video;
-    }
+  public void setVideo(Asset video) {
+    this.video = video;
+  }
 
-    public void setVideo(Asset video) {
-        this.video = video;
-    }
+  public List<Category> getSchools() {
+    return schools;
+  }
 
-    public List<Category> getSchools() {
-        return schools;
-    }
+  public void setSchools(List<Category> schools) {
+    this.schools = schools;
+  }
 
-    public void setSchools(List<Category> schools) {
-        this.schools = schools;
-    }
-
-    public List<Category> getCourseTypes() {
-        return courseTypes;
-    }
-
-    public void setCourseTypes(List<Category> courseTypes) {
-        this.courseTypes = courseTypes;
-    }
 }
